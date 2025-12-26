f762920e7424e1271cad48c17f1024aeb85c575c
GeorgeKhakhiashvili <giorgi.khakhiashvili.1@btu.edu.ge>
2025-12-26 18:00:12 +0400

    Update 6kyu#3.py

--- 6kyu#3.py
+++ 6kyu#3.py
@@ -1,6 +1,12 @@
 #Find the odd int
 def odd(list):
+    #იტერაცია ლისტზე
+    #iterating over list
     for num in list:
+        #ვეძებთ ლისტში იმ ელემნტს რომლის რაოდენობა კენტია
+        #searching  amount of elemnt in list which is odd
         if list.count(num) % 2 != 0:
+            #ვიღებთ სასურველ პასუხს
+            #we get our answer
             return num, "number is odd"
 print(odd([1,2,2,3,3,3,4,3,3,3,2,2,1]))
