f6fa163ddc0055d6a23212a6e80242282757f36b
GeorgeKhakhiashvili <giorgi.khakhiashvili.1@btu.edu.ge>
2025-12-26 18:42:28 +0400

    Update 6kyu#7.py

--- 6kyu#7.py
+++ 6kyu#7.py
@@ -1,12 +1,23 @@
+#ნარცისი რიცხვი/ციფრი მოცემული ციფრი უნდა ემთხვეოდეს ციფრში
+#შემავალი ყველა ნაწილის კუბის ჯამს
 def Narcissistic_Number(txt):
+    #ვაქცევთ სტრინგად, რადგან სტრინგზე იტერაცია უფრო მარტივია
+    #iterating over string is better
     new_txt = str(txt)
+    #ვიგბთ მოცემული სრინგის ზომას, რომელიც იქნება ხარისხი
+    #length of string will be power used later
     power = len(new_txt)
+    #საბოლოო ჯამი
+    #final answer
     sum_of_nums = 0
     for i in new_txt:
+        #საბოლოო ჯამს ვუმატებთ ინტად გადაქცეულ ელენტს ხარისხად (სტრინგის ზომა)
+        #we add sum_of_nums inted element in power of strings length
         sum_of_nums += int(i)**power
+        #ვამოწმებთ მიღებული შედეგი თუ უდრის თვდაპირველ ციფრს/რიცხვს
+        #checking if result and given number is same
     if sum_of_nums == int(txt):
         return True
     else:
         return False
-
-print(Narcissistic_Number('999'))
+print(Narcissistic_Number(999))
